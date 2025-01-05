7ade50565ff6dd27357b01d43c17eec56b5625c8
Kim-soung-won <rlatmddnjs0103@naver.com>
2025-01-05 19:38:55 +0900

    Fix: 응답 유효성 체크 주석 추가

--- src/shared/lib/axios/AxiosContracts.ts
+++ src/shared/lib/axios/AxiosContracts.ts
@@ -6,8 +6,10 @@ export class AxiosContracts {
   // 응답 객체 타입 유효성 확인
   static responseContract<Data>(schema: ZodType<Data>) {
     return (response: AxiosResponse<unknown>): AxiosResponse<Data> => {
+      // 응답이 잘 왔는지 확인
       console.log("response -----> ", response.data);
       const validation = schema.safeParse(response.data);
+      // 유효한 타입 형태인지 확인, success : false 면 타입 체크 필요
       console.log("validation -----> ", validation);
       if (validation.error || !validation.success) {
         throw new AxiosValidationError(
